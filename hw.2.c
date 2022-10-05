093e835e3466e3628f5dcca0d32d5bd18c515a04
garamG <109083994+garamG@users.noreply.github.com>
2022-10-05 22:03:16 +0900

    Update hw.2.c

--- hw.2.c
+++ hw.2.c
@@ -1,11 +1,11 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main() {
-//	double kilo,mile;
-//	printf("please enter kilometers:");
-//	scanf("%lf", &kilo);
-//	mile = kilo / 1.609;
-//	printf("%.1f km is equal to %.1f miles.", kilo, mile);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+  double kilo,mile;
+	printf("please enter kilometers:");
+	scanf("%lf", &kilo);
+	mile = kilo / 1.609;
+	printf("%.1f km is equal to %.1f miles.", kilo, mile);
+	return 0;
+}
