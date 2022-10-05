a3cfafdd574d8a811ec39db5bebb91a914c21497
garamG <109083994+garamG@users.noreply.github.com>
2022-10-05 23:05:21 +0900

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -1,22 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	
-int num;
+int main(){
+	int num;
 printf("please enter a number:");
 scanf("%d", &num); 
-int count=0; 
+int count=0;
 
 for (int i = 2; i < num; i++) {
 	if (num % i == 0) {
-		printf("it is not a prime number");
-		break;
+		count++;
 	}
-	else { printf("it is a prime number\n");
-	     break;
-	     }
-	
+}
+if (count == 0) {
+	printf("it is prime number");
+}
+else { printf("it is not a prime number"); }
+	return 0;
 }
 	return 0;
 }
