8a300446c06fa793e6cbca94c20553ce94b56cbd
garamG <109083994+garamG@users.noreply.github.com>
2022-10-05 23:07:18 +0900

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -3,20 +3,19 @@
 
 int main(){
 	int num;
-printf("please enter a number:");
-scanf("%d", &num); 
-int count=0;
+	printf("please enter a number:");
+	scanf("%d", &num); 
+	int count=0;
 
-for (int i = 2; i < num; i++) {
+	for (int i = 2; i < num; i++) {
 	if (num % i == 0) {
 		count++;
 	}
 }
-if (count == 0) {
+	if (count == 0) {
 	printf("it is prime number");
 }
-else { printf("it is not a prime number"); }
+	else { printf("it is not a prime number"); }
 	return 0;
 }
-	return 0;
-}
+
